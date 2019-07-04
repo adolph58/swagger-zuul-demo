dc6b2b008fec4ab254433067aeaf7f78a89aad9e
chengjiantao <jaunty5@163.com>
2019-07-04 15:36:03 +0800

    增加启动开关

--- order-server/src/main/java/com/example/pay/config/SwaggerConfig.java
+++ order-server/src/main/java/com/example/pay/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.pay.config;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,10 +19,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    //是否开启swagger，正式环境一般是需要关闭的，可根据Springboot的多环境配置进行设置
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                // 是否开启
+                .enable(swaggerEnabled)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example"))
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
